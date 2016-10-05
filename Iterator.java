b3aca841ae08bc34086c96f63236f3db817c7a40
EricYao <ericyao7@gmail.com>
2016-10-05 14:44:54 -0400

    Update Iterator.java

--- Iterator.java
+++ Iterator.java
@@ -1,3 +1,19 @@
+public String count(String s, int k){
+    int[] count = new int[26];
+    for(int i=0; i<s.length(); i++){
+        char c = s.charAt(i);
+        count[c-'a']++;
+    }
+    StringBuilder sb = new StringBuilder();
+    for(int i=0; i<count.length; i++){
+        if(count[i] >0 && count[i] <k)
+            sb.append((char)(i+'a'));
+    }
+    return sb.toString();
+}
+
+
+
 Iterator entries = m.entrySet().iterator();
 
         while (entries.hasNext()){
